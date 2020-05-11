a0608c514c87e45e357b5b25a2c84c5b2beef038
alanhaziev324 <56869776+alanhaziev324@users.noreply.github.com>
2020-05-11 22:50:17 +0300

    Update start3.py

--- start3.py
+++ start3.py
@@ -1,3 +1,4 @@
+import os
 from collections import namedtuple
 
 from flask import Flask, render_template, redirect, url_for, request
@@ -44,4 +45,5 @@ def add_message():
 
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
